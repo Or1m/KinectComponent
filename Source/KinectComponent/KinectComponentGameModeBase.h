d3faff0c1e91ba4273904abb48758a3ea0ec59c0
Miroslav Kačeriak <k.z.w.212@gmail.com>
2021-08-22 12:26:07 +0200

    Initial commit

---
+++ Source/KinectComponent/KinectComponentGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KinectComponentGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KINECTCOMPONENT_API AKinectComponentGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
