e9645c80278cc015b8b43ad0ac8e62955fc8697b
Miro Kačeriak <miro.kaceriak@peruncreative.com>
2022-01-29 14:43:15 +0100

    Refactor

--- Source/KinectComponent/ProceduralActor.cpp
+++ Source/KinectComponent/ProceduralActor.cpp
@@ -1,41 +1,9 @@
-// Copyright 2021, Bc. Miroslav Kaèeriak. All Rights Reserved.
-
 #include "ProceduralActor.h"
+
 #include <ProceduralMeshComponent/Public/KismetProceduralMeshLibrary.h>
-#include <iostream>
-#include "Components/SphereComponent.h"
 #include "Components/StaticMeshComponent.h"
-#include <ctime>
+#include "Components/SphereComponent.h"
 
-
-struct HeightMapType
-{
-    float x, y, z;
-    float nx, ny, nz;
-    float r, g, b;
-};
-
-constexpr unsigned int KINECT_DEPTH_WIDTH = 512;
-constexpr unsigned int KINECT_DEPTH_HEIGHT = 424;
-constexpr unsigned int KINECT_DEPTH_CAPACITY = KINECT_DEPTH_WIDTH * KINECT_DEPTH_HEIGHT;
-constexpr unsigned int FRAME = KINECT_DEPTH_CAPACITY * sizeof(UINT16);
-
-constexpr unsigned int MAX = 7908;
-
-FString absoluteFilePath = FPaths::ProjectDir() + TEXT("kinectDepthData_0.raw");
-std::string stringPath = std::string(TCHAR_TO_UTF8(*absoluteFilePath));
-
-HeightMapType* HEIGHTMAP;
-UINT16* rawImage;
-FILE* filePtr;
-
-inline float Normalize(float val, int max, int min) { return (val - min) / (max - min); }
-
-inline void WaitSecond()
-{
-    time_t before = time(0);
-    while (difftime(time(0), before) < 1);
-}
 
 AProceduralActor::AProceduralActor()
 {
@@ -48,12 +16,13 @@ AProceduralActor::AProceduralActor()
 	InitializeInGameMesh();
 }
 
+
 void AProceduralActor::CreateEditorPlaceHolder()
 {
 	editorMash = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Placeholder"));
 
-	ConstructorHelpers::FObjectFinder<UStaticMesh>	meshAsset(TEXT("StaticMesh'/Engine/BasicShapes/Plane'"));
-	ConstructorHelpers::FObjectFinder<UMaterial>	material(TEXT("Material'/Game/Materials/Material'"));
+	ConstructorHelpers::FObjectFinder<UStaticMesh> meshAsset(TEXT("StaticMesh'/Engine/BasicShapes/Plane'"));
+	ConstructorHelpers::FObjectFinder<UMaterial> material(TEXT("Material'/Game/Materials/Material'"));
 
 	editorMash->SetStaticMesh(meshAsset.Object);
 	editorMash->GetStaticMesh()->SetMaterial(0, material.Object);
@@ -74,7 +43,8 @@ void AProceduralActor::BeginPlay()
 
     PrimaryActorTick.SetTickFunctionEnable(tick);
 
-    if (LoadHeightMap()) {
+    if (LoadHeightMap()) 
+    {
         GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, "Loaded");
         CreateMesh();
     }
@@ -94,23 +64,22 @@ void AProceduralActor::Tick(float DeltaTime)
 
     if (currentTime >= updateInterval)
     {
-        //GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, TEXT("Tick"));
         currentTime = 0.0f;
 
         int length = vertices.Num();
 
         if (!feof(filePtr)) 
         {
-            unsigned long long count = fread(rawImage, sizeof(UINT16), KINECT_DEPTH_CAPACITY, filePtr);
+            count = fread(rawImage, sizeof(UINT16), KINECT_DEPTH_CAPACITY, filePtr);
 
             if (count != KINECT_DEPTH_CAPACITY)
             {
-                GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, TEXT("tick count != imageSize"));
+                GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, TEXT("tick: count != imageSize"));
             }
 
             for (int i = 0; i < length; i++)
             {
-                vertices[i].Z = Normalize((float)rawImage[i], MAX, 0) * HeightMultiplicator;
+                vertices[i].Z = Normalize((float)rawImage[i]) * HeightMultiplicator;
             }
 
             UpdateMesh();
@@ -121,21 +90,13 @@ void AProceduralActor::Tick(float DeltaTime)
 
 bool AProceduralActor::LoadHeightMap()
 {
-    unsigned long long count;
-    int error;
-
-    HEIGHTMAP = new HeightMapType[KINECT_DEPTH_WIDTH * KINECT_DEPTH_HEIGHT];
-    if (!HEIGHTMAP)
-    {
-        GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, TEXT("!HEIGHTMAP"));
-        return false;
-    }
+    const char* filePath = TCHAR_TO_UTF8(*(FPaths::ProjectDir() + fileName));
 
     // Open the 16 bit raw height map file for reading in binary.
-    error = fopen_s(&filePtr, stringPath.c_str(), "rb");
+    int error = fopen_s(&filePtr, filePath, "rb");
     if (error != 0)
     {
-        GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, TEXT("ERR in opening " + absoluteFilePath));
+        GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, TEXT("ERR in opening file"));
         return false;
     }
 
@@ -169,7 +130,9 @@ bool AProceduralActor::UnloadHeightMap() {
 
     // Release the bitmap image data.
     delete[] rawImage;
-    rawImage = 0;
+
+    rawImage = nullptr;
+    filePtr = nullptr;
 
     return true;
 }
@@ -189,7 +152,7 @@ void AProceduralActor::CreateMesh()
 
             item.X = i * LengthMultiplicator;
             item.Y = j * LengthMultiplicator;
-            item.Z = Normalize((float)rawImage[index], MAX, 0) * HeightMultiplicator; //static_cast<float>((rand() / static_cast<float>(RAND_MAX)) * HeightMultiplicator);
+            item.Z = Normalize((float)rawImage[index]) * HeightMultiplicator; //static_cast<float>((rand() / static_cast<float>(RAND_MAX)) * HeightMultiplicator);
             
             vertices.Add(item);
         }
@@ -204,4 +167,10 @@ void AProceduralActor::UpdateMesh()
 
 	mesh->CreateMeshSection_LinearColor(0, vertices, triangles, normals, uvs, vertexColors, tangents, true);
 	mesh->ContainsPhysicsTriMeshData(true); // Enable collision data
+}
+
+
+inline float AProceduralActor::Normalize(const float value) 
+{ 
+    return (value - MIN) / (MAX - MIN); 
 }
--- Source/KinectComponent/ProceduralActor.h
+++ Source/KinectComponent/ProceduralActor.h
@@ -1,6 +1,6 @@
-// Copyright 2021, Bc. Miroslav Kaèeriak. All Rights Reserved.
-
 #pragma once
+
+
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
@@ -12,8 +12,7 @@ class KINECTCOMPONENT_API AProceduralActor : public AActor
 {
 	GENERATED_BODY()
 	
-public:	
-	// Sets default values for this actor's properties
+public:
 	AProceduralActor();
 
 	UPROPERTY(EditAnywhere, Category = Attributes)
@@ -27,21 +26,33 @@ public:
 	UPROPERTY(EditAnywhere, Category = Debug)
 		float updateInterval = 1.0f;
 
+	const FString fileName = "kinectDepthData_0.raw";
+
+	// Kinect constants
+	static constexpr unsigned int MIN = 0;
+	static constexpr unsigned int MAX = 8192; // 2^13
+
+	static constexpr unsigned int KINECT_DEPTH_WIDTH = 512;
+	static constexpr unsigned int KINECT_DEPTH_HEIGHT = 424;
+
+	static constexpr unsigned int KINECT_DEPTH_CAPACITY = KINECT_DEPTH_WIDTH * KINECT_DEPTH_HEIGHT;
+	static constexpr unsigned int FRAME = KINECT_DEPTH_CAPACITY * sizeof(UINT16);
+
 protected:
+	void CreateEditorPlaceHolder();
+	void InitializeInGameMesh();
+
 	bool LoadHeightMap();
 	bool UnloadHeightMap();
-	void InitializeInGameMesh();
+
 	void CreateMesh();
 	void UpdateMesh();
 
-	void CreateEditorPlaceHolder();
-
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	// Called when the game starts or when spawned
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	inline float Normalize(const float value);
 
 private:
 	UPROPERTY()
@@ -62,6 +73,11 @@ private:
 	UPROPERTY()
 		TArray<FProcMeshTangent> tangents;
 
+	UINT16* rawImage;
+	FILE* filePtr;
+
 	float currentTime = 0.0f;
+	unsigned long long count;
+
 	bool tick;
 };
